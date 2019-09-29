0aaf857c64712d21e42b50379c83b24d84bdf06c
Stanislav Humeniuk <stanislav.gumeniuk@4k.com.ua>
2019-09-29 13:06:32 +0300

    Create Repository for work with api and local database, add it in MainActivity

--- app/src/main/java/com/knightua/notepadapp/mvp/views/activity/MainActivity.kt
+++ app/src/main/java/com/knightua/notepadapp/mvp/views/activity/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.knightua.notepadapp.mvp.views.activity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import com.knightua.basemodule.abstracts.view.BaseCompatActivity
 import com.knightua.notepadapp.R
-import com.knightua.notepadapp.di.application.NotepadMvpApp
+import com.knightua.notepadapp.di.application.NotepadApp
 import com.knightua.notepadapp.mvp.contracts.MainActivityContract
 import com.knightua.notepadapp.mvp.presenters.MainActivityPresenter
+import com.knightua.notepadapp.reposotories.NoteRepository
 import javax.inject.Inject
 
 class MainActivity : BaseCompatActivity(), MainActivityContract.View {
@@ -13,9 +15,18 @@ class MainActivity : BaseCompatActivity(), MainActivityContract.View {
     @Inject
     lateinit var presenter: MainActivityPresenter
 
-    override fun init(savedInstanceState: Bundle?) {
+    @Inject
+    lateinit var noteRepository: NoteRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
+
+    @SuppressLint("CheckResult")
+    fun init() {
         setContentView(R.layout.activity_main)
-        NotepadMvpApp.get().injector.inject(this)
+        NotepadApp.get().injector.inject(this)
         presenter.attach(this)
     }
 }
---
+++ app/src/main/java/com/knightua/notepadapp/reposotories/NoteRepository.kt
@@ -0,0 +1,130 @@
+package com.knightua.notepadapp.reposotories
+
+import android.annotation.SuppressLint
+import com.knightua.notepadapp.network.WebServer
+import com.knightua.notepadapp.room.dao.NoteDao
+import com.knightua.notepadapp.room.entity.Note
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
+class NoteRepository(private val noteDao: NoteDao, private val webServer: WebServer) {
+
+    fun getAll(): Observable<List<Note>> {
+        return Observable.concatArray(
+            getAllFromDatabase(),
+            getAllFromApi()
+        )
+    }
+
+    fun getAllFromDatabase(): Observable<List<Note>> {
+        return noteDao.getAll()
+            .filter {
+                it.isNotEmpty()
+            }
+            .toObservable()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext {
+                Timber.d("Dispatching ${it.size} notes from DataBase...")
+            }
+    }
+
+    fun getAllFromApi(): Observable<List<Note>> {
+        return webServer.getAllNotes()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .delay(3, TimeUnit.SECONDS)
+            .doOnNext {
+                Timber.d("Dispatching ${it.size} notes from Api...")
+                insertAllInDatabase(it)
+            }
+    }
+
+    fun getByIdFromDatabase(id: Long): Observable<Note> {
+        return noteDao.getById(id)
+            .toObservable()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext {
+                Timber.d("Dispatching ${it} notes from DataBase...")
+            }
+    }
+
+    @SuppressLint("CheckResult")
+    fun insertAllInDatabase(notes: List<Note>) {
+        Single.fromCallable { noteDao.insertAll(notes) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { Timber.d("Inserted ${notes.size} notes in DataBase") },
+                { Timber.e(it.toString()) })
+    }
+
+    @SuppressLint("CheckResult")
+    fun insertInDatabase(note: Note) {
+        Single.fromCallable { noteDao.insert(note) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { Timber.d("Insert ${note} in DataBase") },
+                { Timber.e(it.toString()) })
+    }
+
+    @SuppressLint("CheckResult")
+    fun updateInDatabase(note: Note) {
+        Single.fromCallable { noteDao.update(note) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { Timber.d("Update ${note} in DataBase") },
+                { Timber.e(it.toString()) })
+    }
+
+    @SuppressLint("CheckResult")
+    fun deleteAllFromDatabase(notes: List<Note>) {
+        Single.fromCallable { noteDao.deleteAll(notes) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { Timber.d("Deleted ${notes.size} from DataBase") },
+                { Timber.e(it.toString()) }
+            )
+    }
+
+    @SuppressLint("CheckResult")
+    fun deleteFromDatabase(note: Note) {
+        Single.fromCallable { noteDao.delete(note) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { Timber.d("Delete ${note} from DataBase") },
+                { Timber.e(it.toString()) }
+            )
+    }
+
+    @SuppressLint("CheckResult")
+    fun deleteFromDatabase(id: Long) {
+        Single.fromCallable { noteDao.deleteById(id) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { Timber.d("Delete note by ${id} id from DataBase") },
+                { Timber.e(it.toString()) }
+            )
+    }
+
+    @SuppressLint("CheckResult")
+    fun clearDatabase() {
+        Single.fromCallable { noteDao.clear() }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { Timber.d("Clear all notes from DataBase") },
+                { Timber.e(it.toString()) }
+            )
+    }
+}
