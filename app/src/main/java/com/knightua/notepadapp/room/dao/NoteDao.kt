a97c0d0b0da895c56ac2961d83061430ce6e85c5
Stanislav Humeniuk <stanislav.gumeniuk@4k.com.ua>
2019-09-29 13:04:45 +0300

    Add observables for methods in Dao

--- app/src/main/java/com/knightua/notepadapp/room/dao/NoteDao.kt
+++ app/src/main/java/com/knightua/notepadapp/room/dao/NoteDao.kt
@@ -2,28 +2,35 @@ package com.knightua.notepadapp.room.dao
 
 import androidx.room.*
 import com.knightua.notepadapp.room.entity.Note
+import io.reactivex.Single
 
 @Dao
 interface NoteDao {
 
     @Query("SELECT * FROM note")
-    fun getAll(): List<Note>
+    fun getAll(): Single<List<Note>>
 
     @Query("SELECT * FROM note WHERE id = :id")
-    fun getById(id: Long): Note
+    fun getById(id: Long): Single<Note>
 
-    @Insert
-    fun insertAll(vararg notes: Note)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(notes: List<Note>)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(note: Note)
 
     @Update
     fun update(note: Note)
 
     @Delete
-    fun deleteAll(vararg notes: Note)
+    fun deleteAll(notes: List<Note>)
 
     @Delete
     fun delete(note: Note)
+
+    @Query("DELETE FROM note WHERE id = :id")
+    fun deleteById(id: Long)
+
+    @Query("DELETE FROM note")
+    fun clear()
 }
