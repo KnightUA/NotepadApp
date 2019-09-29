b0efbc4bce26751746d3b360b86945456d289d72
Stanislav Humeniuk <stanislav.gumeniuk@4k.com.ua>
2019-09-29 13:05:40 +0300

    Create WebServer with creating default notes, add dependency in room module

--- app/src/main/java/com/knightua/notepadapp/di/modules/RoomModule.kt
+++ app/src/main/java/com/knightua/notepadapp/di/modules/RoomModule.kt
@@ -2,6 +2,8 @@ package com.knightua.notepadapp.di.modules
 
 import android.app.Application
 import androidx.room.Room
+import com.knightua.notepadapp.network.WebServer
+import com.knightua.notepadapp.reposotories.NoteRepository
 import com.knightua.notepadapp.room.dao.NoteDao
 import com.knightua.notepadapp.room.database.NotepadAppDatabase
 import dagger.Module
@@ -15,6 +17,8 @@ class RoomModule(mApplication: Application) {
     private var notepadAppDatabase: NotepadAppDatabase =
         Room.databaseBuilder(mApplication, NotepadAppDatabase::class.java, "notepad-db").build()
 
+    private var webServer = WebServer()
+
     @Singleton
     @Provides
     fun providesRoomDatabase(): NotepadAppDatabase = notepadAppDatabase
@@ -23,4 +27,9 @@ class RoomModule(mApplication: Application) {
     @Provides
     fun providesNoteDao(notepadAppDatabase: NotepadAppDatabase): NoteDao =
         notepadAppDatabase.noteDao()
+
+    @Singleton
+    @Provides
+    fun providesNoteRepository(noteDao: NoteDao): NoteRepository =
+        NoteRepository(notepadAppDatabase.noteDao(), webServer)
 }
